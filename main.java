779cf97a1081a00cd167739896599bd36d799d4d
Martin Maslein <tincho_m98@hotmail.com>
2020-09-17 11:10:28 -0300

    commit conflicto

--- main.java
+++ main.java
@@ -2,7 +2,7 @@ public class main {
 	
 	public static void main(String [] args) {
 		
-		Persona p = new Persona("Martin");
+		Persona p = new Persona("Conflicto");
 		System.out.println(p.getNombre());
 	}
 }
