3a12e7bb62a46babfabdc8b396362eba61ad5ab4
Martin Maslein <tincho_m98@hotmail.com>
2020-09-17 11:03:28 -0300

    primer commit

---
+++ Persona.java
@@ -0,0 +1,16 @@
+public class Persona {
+	
+	private String nombre;
+
+	public Persona(String n) {
+		nombre = n;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String n) {
+		nombre = n
+	}
+}
